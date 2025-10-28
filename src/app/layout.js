419dafa30b0743439ff64ef1dd4ca7c361d56ca9
Lhawhalhe001 <onabiyiabdulsamod@gmail.com>
2025-10-28 07:33:45 +0100

    Changed description

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,18 +4,16 @@ import "./globals.css";
 
 export const metadata = {
   title: "Ace Jupeb Learning Portal",
-  description: "A simple JUPEB learning portal built with Next.js",
-   icons: {
-    icon: "/favicon.png",  // 👈 This line makes it appear beside the site link
+  description: "A simple JUPEB learning portal to ace your learning journey.",
+  icons: {
+    icon: "/favicon.png", // 👈 This line makes it appear beside the site link
   },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
